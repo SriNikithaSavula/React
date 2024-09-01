66eda6bd2fc397671d59c8b0609f9762c747a592
SriNikithaSavula <727723eucs240@skcet.ac.in>
2024-09-01 23:37:02 +0530

    Update DAY-1CY-1.jsx

--- DAY-1CY-1.jsx
+++ DAY-1CY-1.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 function Compare()
 {
-    const name1="Kabil";
-    const name2="Kabil";
-    const object1={name:"Kabil"};
-   const object2={name:"Kabil"};
+    const name1="Krish";
+    const name2="Krish";
+    const object1={name:"Krish"};
+   const object2={name:"Krish"};
    alert(object1===object2);
    alert(name1===name2);
    alert(object1===name1);
 }
-const Harish = ()=>{
+const Nikil = ()=>{
     return (
         <div>
             <h1>Let we see the output of javascript</h1>
@@ -17,4 +17,4 @@ const Harish = ()=>{
         </div>
     );
 };
-export default Harish;
+export default Nikil;
