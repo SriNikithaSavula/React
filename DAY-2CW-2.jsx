0bff57fd9fc1223da523b81e054e0ac78cfaaaff
SriNikithaSavula <727723eucs240@skcet.ac.in>
2024-09-01 23:50:36 +0530

    Update DAY-2CW-2.jsx

--- DAY-2CW-2.jsx
+++ DAY-2CW-2.jsx
@@ -2,9 +2,9 @@ import React from "react";
 const Message=()=> {
     return (
         <div>
-            <h1 style={{color:'green'}}>Inline Style in JSX Example.</h1>
-            <div style={{background:'lightblue',color:'darkblue',padding:'10px',border:'1px solid blue.',borderRadius:'5px'}}>
-            <p style={{color:'darkblue',fontSize:'16px'}}>This is a paragraph with inline styles applied.</p></div>
+            <h1 style={{color:'red'}}>Inline Style in JSX Example.</h1>
+            <div style={{background:'white',color:'black',padding:'10px',border:'1px solid blue.',borderRadius:'5px'}}>
+            <p style={{color:'black',fontSize:'16px'}}>This is a paragraph with inline styles applied.</p></div>
         </div>
     );
 };
