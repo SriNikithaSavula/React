64b1489f6c21cf681d9d164d00e4a8985568a83b
SriNikithaSavula <727723eucs240@skcet.ac.in>
2024-09-02 00:04:00 +0530

    Update DAY-2DC-1.jsx

--- DAY-2DC-1.jsx
+++ DAY-2DC-1.jsx
@@ -3,8 +3,8 @@ const Car=() =>{
   return(
     <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',backgroundColor:'lightskyblue'}}>
       <h1 >Car and Location Info</h1>
-      <p style={{textAlign:'center',backgroundColor:'Red',padding:'100px',fontFamily:'monospace',fontSize:'30px'}} >Porsche Model <br></br>
-      <img src="https://imgd-ct.aeplcdn.com/1056x660/n/xsoupsa_1476828.jpg?q=80" style={{ width:'500px',height:'300px'}}></img>
+      <p style={{textAlign:'center',backgroundColor:'Red',padding:'100px',fontFamily:'monospace',fontSize:'30px'}} >Thar Model <br></br>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/1/13/Mahindra_Thar_Photoshoot_At_Perupalem_Beach_%28West_Godavari_District%2CAP%2CIndia_%29_Djdavid.jpg" style={{ width:'500px',height:'300px'}}></img>
       </p>
     </div>
   );
